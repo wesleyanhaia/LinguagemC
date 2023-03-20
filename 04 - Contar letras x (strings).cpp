1907270ce1402aeb749a08b7cbe028e560728e7d
Wesley Anhaia <116120620+wesleyanhaia@users.noreply.github.com>
2023-03-20 20:34:05 -0300

    Add files via upload

---
+++ 04 - Contar letras x (strings).cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+
+
+main(){
+	int i, cont=0, tamanho=0;
+	char frase[30];
+	
+	printf("Digite uma frase: ");
+	gets(frase);
+	
+	tamanho=strlen(frase);
+	
+	for(i=0; i<tamanho; i++){
+		
+		if(toupper(frase[i])=='X'){
+			
+			cont++;
+		}
+	}
+	
+	printf("Aquantidade de letras x nessa frase eh de %d", cont);
+}
