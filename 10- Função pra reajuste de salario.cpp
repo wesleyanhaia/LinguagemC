cadd5e6222faed9f5345dfe46d7d8dece176bd6d
Wesley Anhaia <116120620+wesleyanhaia@users.noreply.github.com>
2023-04-10 19:55:42 -0300

    Update 10- Função pra reajuste de salario.cpp

--- 10- Função pra reajuste de salario.cpp
+++ 10- Função pra reajuste de salario.cpp
@@ -1,3 +1,8 @@
+/*Uma empresa decide dar um aumento de 30% aos funcionários com salários inferiores
+a R$ 500,00. Faça uma função que receba o salário do funcionário e mostre o valor do
+salário reajustado ou uma mensagem, caso o funcionário não tenha direito ao aumento. A
+mensagem deve ser uma outra função.*/
+
 #include <stdio.h>
 void funcao(float salario);
 void mensagem();
